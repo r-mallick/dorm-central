684ef0806679b9e51eef9960ba610cfdd48495bb
r-mallick <rmallick0155@gmail.com>
2023-02-11 20:24:18 -0800

    Update App.js 2.0
    Testing out whether MaterialUI works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div>
-      <header>
-        Dorm Central
-      </header>
+      <Button variant="contained">Example Material UI button</Button>
+      This is Dorm Central
     </div>
   );
 }
