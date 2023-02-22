c21a0f3d205740a025b015ba3c49b130693e5cef
Aleah delagdo <aleahdelagdo@s-169-232-117-167.resnet.ucla.edu>
2023-02-21 19:41:25 -0800

    added react-router and linked homepage buttons to pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 import Button from '@mui/material/Button';
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home/Home';
+import Comparison from './pages/Comparison/Comparison';
+import Party from './pages/Party/Party';
+import ReviewsPage from './pages/ReviewsPage/ReviewsPage';
+import ReviewSubmission from './pages/ReviewSubmission/ReviewSubmission';
 
 function App() {
   return (
-    <div>
-      <Button variant="contained">Example Material UI button</Button>
-      This is Dorm Central
-    </div>
+    // <div>
+    //   <Button variant="contained">Example Material UI button</Button>
+    //   This is Dorm Central
+    // </div>
+    
+    <Routes>
+        <Route index element={<Home />} />
+        <Route path='comparison' element={<Comparison />} />
+        <Route path='reviewsPage' element={<ReviewsPage />} />
+        <Route path='reviewSubmission' element={<ReviewSubmission />} />
+        <Route path='party' element={<Party />} />
+
+    </Routes>
+  
   );
 }
 
